4e73b5ffc64adc4f4392326a3cd47b63a325afff
violg <violg@yandex-team.ru>
2018-01-01 12:04:35 +0500

    initial commit

---
+++ Elliptic-Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Elliptic-Swift
+//
+//  Created by Olga Vyrostko on 01.01.18.
+//  Copyright © 2018 OlgaVyrostko. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
